f1cd062d082157bb9ea2b358bd1b00daee091052
Jackson Greaves <jdgreave@uci.edu>
2019-03-15 17:36:00 -0700

    Added some assets and made a dummy script

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    public int speedMod = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("W"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+}

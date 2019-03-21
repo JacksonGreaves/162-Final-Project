d07084504897c5676fd4bc7742f8fc8205c652ef
Zachary Cloutier <zcloutie@uci.edu>
2019-03-20 23:27:46 -0700

    Cannons Fire and bullets explode on contact with objects

---
+++ Project/FINAL/Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private GameObject water;
+    private Rigidbody rb;
+    private MeshRenderer rend;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<MeshRenderer>();
+        water = GameObject.Find("Sea");
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -100)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject != water)
+        {
+            rend.enabled = false;
+            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+            var exp = GetComponentInChildren<ParticleSystem>();
+            exp.Play();
+            Destroy(gameObject, exp.duration);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Project/FINAL/Assets/Scripts/ShipController.cs
+++ Project/FINAL/Assets/Scripts/ShipController.cs
@@ -6,8 +6,12 @@ public class ShipController : MonoBehaviour
 {
     public float speed = 3;
     public float rotateSpeed = 3;
+    public int shot_speed = 10;
+    public GameObject[] l_cannons;
+    public GameObject[] r_cannons;
+    public GameObject bullet;
 
-	private CharacterController controller;
+    private CharacterController controller;
     private float moveX;
     private float moveZ;
     // Start is called before the first frame update
@@ -23,5 +27,22 @@ public class ShipController : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire(l_cannons);
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Fire(r_cannons);
+        }
+    }
+
+    void Fire(GameObject[] cannons)
+    {
+        foreach (GameObject cannon in cannons){
+            GameObject ballClone = Instantiate(bullet, cannon.transform.position, cannon.transform.rotation);
+            Rigidbody ballClone_rb = ballClone.GetComponent<Rigidbody>();
+            ballClone_rb.AddForce(cannon.transform.forward * shot_speed);
+        }
     }
 }

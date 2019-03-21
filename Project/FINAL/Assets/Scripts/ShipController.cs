d2e13c86b3719ba160fb855ba054d9249ab17847
Zachary Cloutier <zcloutie@uci.edu>
2019-03-20 21:19:05 -0700

    Base Work in

---
+++ Project/FINAL/Assets/Scripts/ShipController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotateSpeed = 3;
+
+	private CharacterController controller;
+    private float moveX;
+    private float moveZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
